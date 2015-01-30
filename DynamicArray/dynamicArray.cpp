32012e10b73e452abb9d6e91aaa832a17a55bf39
Francois Chantal <geniousmouton@gmail.com>
2015-01-30 11:16:50 -0400

    TEST: valeur default des element est 0

--- DynamicArray/dynamicArray.cpp
+++ DynamicArray/dynamicArray.cpp
@@ -1,11 +1,17 @@
 #include "dynamicArray.h"
 
-DynamicArray::DynamicArray(unsigned _capacite)
+DynamicArray::DynamicArray(const unsigned int _capacite)
 {
 	capacite = _capacite;
+	tabElements = new int[_capacite] {};
 }
 
 int DynamicArray::getCapacite() const
 {
 	return capacite;
+}
+
+int DynamicArray::getElement(const unsigned i)
+{
+	return tabElements[i];
 }
--- DynamicArray/dynamicArray.h
+++ DynamicArray/dynamicArray.h
@@ -1,16 +1,17 @@
 #ifndef DYNAMICARRAY_H_
 #define DYNAMICARRAY_H_
 
-
 class DynamicArray
 {
 public:
 	DynamicArray(unsigned int _capacite = 100);
 	int getCapacite() const;
+	int getElement(const unsigned int i);
 
 private:
 	int* tabElements;
 	unsigned int capacite;
+
 	
 };
 
